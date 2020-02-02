2ef43811385a82a84e4aa94a6af3ac518ce5b985
Alexander Lotz <alexandermlotz@gmail.com>
2020-02-02 00:05:52 -0500

    Updated Nez path to be included in repo for portability.

---
+++ Nez/Nez.Portable/Input/InputUtils.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+
+namespace Nez
+{
+	public static class InputUtils
+	{
+		public static bool IsMac;
+		public static bool IsWindows;
+		public static bool IsLinux;
+
+
+		static InputUtils()
+		{
+			IsMac = true;
+		}
+
+
+		public static bool IsShiftDown()
+		{
+			return Input.IsKeyDown(Keys.LeftShift) || Input.IsKeyDown(Keys.RightShift);
+		}
+
+
+		public static bool IsAltDown()
+		{
+			return Input.IsKeyDown(Keys.LeftAlt) || Input.IsKeyDown(Keys.RightAlt);
+		}
+
+
+		public static bool IsControlDown()
+		{
+			if (IsMac)
+				return Input.IsKeyDown(Keys.LeftWindows) || Input.IsKeyDown(Keys.RightWindows);
+
+			return Input.IsKeyDown(Keys.LeftControl) || Input.IsKeyDown(Keys.RightControl);
+		}
+	}
+}
